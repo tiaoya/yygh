9e14dac456a5ca678ac83e2b7a515fc59bdc39cf
zhong <1759397117@qq.com>
2022-10-28 12:38:16 +0800

    医院列表展示,启动分页

--- src/views/yygh/hospset/list.vue
+++ src/views/yygh/hospset/list.vue
@@ -1,51 +1,91 @@
 <template>
   <div class="app-container">
-     <el-table
-        :data="rows"
-        stripe
-        style="width: 100%">
-        
-        <el-table-column
-        prop="id"
-        label="id"
-        width="180">
-        </el-table-column>
-        
-        <el-table-column
-        prop="createTime"
-        label="创建时间"
-        width="180">
-        </el-table-column>
-        
-        <el-table-column
-        prop="updateTime"
-        label="创建时间">
-        </el-table-column>
+    <el-table
+      v-loading="listLoading"
+      :data="list"
+      element-loading-text="数据加载中"
+      border
+      fit
+      highlight-current-row
+    >
+      <el-table-column label="序号" width="70" align="center">
+        <template slot-scope="scope">
+          {{ (page - 1) * limit + scope.$index + 1 }}
+        </template>
+      </el-table-column>
+      <el-table-column prop="hosname" label="医院名称" width="180" />
+      <el-table-column prop="hoscode" label="医院编号" width="160" />
+      <el-table-column prop="apiUrl" label="地址" width="200" />
+      <el-table-column prop="contactsName" label="联系人" />
+      <el-table-column prop="status" label="状态">
+        <template slot-scope="scope">
+          {{ scope.row.status === 1 ? "可用" : "不可用" }}
+        </template>
+      </el-table-column>
+
+      <el-table-column label="操作" width="200" align="center">
+        <template slot-scope="scope">
+          <router-link :to="'/yygh/hospset/edit/' + scope.row.id">
+            <el-button type="primary" size="mini" icon="el-icon-edit"
+              >修改</el-button
+            >
+          </router-link>
+          <el-button
+            type="danger"
+            size="mini"
+            icon="el-icon-delete"
+            @click="removeDataById(scope.row.id)"
+            >删除</el-button
+          >
+        </template>
+      </el-table-column>
     </el-table>
-  
+    <!-- 分页 -->
+    <el-pagination
+      :current-page="page"
+      :page-size="limit"
+      :total="total"
+      style="padding: 30px 0; text-align: center"
+      layout="total, prev, pager, next, jumper"
+      @current-change="handleCurrentChange"
+    />
   </div>
 </template>
 <script>
-  import hospset  from '@/api/hospset.js'
-  export default {
-      data(){
-          return {
-              //变量：值
-              rows:[],
-              total:0,
-              searchObj:{}
-          }
-      },
-      methods:{
+import hospset from "@/api/hospset.js";
+export default {
+  data() {
+    return {
+      //变量：值
+      list: [],
+      total: 0,
+      searchObj: {},
+      listLoading: false,
+      page: 1,
+      limit: 3,
+    };
+  },
+  methods: {
+    // es6 新特性，可以在参数位置添加默认值
+    getPageInfo(val = 1) {
+      this.page = val;
+      hospset
+        .getHospsetPage(this.page, this.limit, this.searchObj)
+        .then((resp) => {
+          this.total = resp.data.total;
+          this.list = resp.data.rows;
+        });
+    },
 
-      },
-      created(){
-          hospset.getHospsetPage(1,5,this.searchObj).then(resp=>{
-              this.total=resp.data.total;
-              this.rows=resp.data.rows;
-          })
-      }
+    handleCurrentChange(val){
+        this.getPageInfo(val);
+    }
 
-  }
+  },
+
+  created() {
+    this.getPageInfo();
+  },
+};
 </script>
 
