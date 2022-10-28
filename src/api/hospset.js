dc02ba5aa1c52e9b2dbbfcc48f81b7c4cde38da1
zhong <1759397117@qq.com>
2022-10-28 23:16:20 +0800

    增加医院添加,表单校验规则,

--- src/api/hospset.js
+++ src/api/hospset.js
@@ -5,19 +5,32 @@ import request from '@/utils/request'
 const API="/admin/hosp/hospitalSet";
 //es6导出语法
 export default {
+
+    // 带查询条件条件的分页
     getHospsetPage(pageNum,size,searchObj) {
         return request({
           url: `${API}/page/${pageNum}/${size}`,
           method: 'post',
           //如果携带的是普通参数：params,如果携带的是json数据：data
           data:searchObj
-        })
-      },
+      })
+    },
 
-      removeDataById(id){
+      // 根据id 删除某个医院设置信息
+    removeDataById(id){
         return request({
           url: `${API}/deleteById/${id}`,
           method: 'delete'
-        })
-      }
+      })
+    },
+
+    // 医院添加
+    addHospital(obj){
+      return request({
+        url:`${API}/save`,
+        method:'post',
+        data : obj
+      })
+    }
+
 }
--- src/views/yygh/hospset/save.vue
+++ src/views/yygh/hospset/save.vue
@@ -1,8 +1,80 @@
 <template>
   <div class="app-container">
-      医院添加|修改页面
+    <el-form label-width="120px" :rules="rulesx" :model="hospset" ref="hospset">
+      <el-form-item label="医院名称" prop="hosname">
+        <el-input v-model="hospset.hosname" />
+      </el-form-item>
+
+      <el-form-item label="医院编号">
+        <el-input v-model="hospset.hoscode" />
+      </el-form-item>
+
+      <el-form-item label="api地址">
+        <el-input v-model="hospset.apiUrl" />
+      </el-form-item>
+
+      <el-form-item label="联系人">
+        <el-input v-model="hospset.contactsName" />
+      </el-form-item>
+
+      <el-form-item label="电话" prop="contactsPhone">
+        <el-input v-model="hospset.contactsPhone" />
+      </el-form-item>
+
+      <el-form-item>
+        <!-- @click="saveOrUpdate('hospset')" 里面的参数是做二次校验用的 -->
+        <el-button
+          :disabled="saveBtnDisabled"
+          type="primary"
+          @click="saveOrUpdate('hospset')"
+          >保存</el-button
+        >
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 <script>
+import hospset from "@/api/hospset.js";
+export default {
+  data() {
+    return {
+      saveBtnDisabled: false,
+      hospset: {},
+      rulesx: {
+        hosname: [
+          { required: true, message: "请输入医院名称", trigger: "blur" },
+          {
+            min: 3,
+            max: 10,
+            message: "长度在 3 到 10 个字符",
+            trigger: "blur",
+          },
+        ],
+        contactsPhone: [
+          { required: true, message: "请输入手机号", trigger: "blur" },
+          { min: 11, max: 11, message: "请输入正确的手机号", trigger: "blur" },
+        ],
+      },
+    };
+  },
+  methods: {
+    saveOrUpdate(formName) {
+      this.$refs[formName].validate((valid) => {
+
+        if (valid) {
+          this.saveBtnDisabled = true;
+          hospset.addHospital(this.hospset).then((resp) => {
+            this.$message.success("添加成功");
+            // 跳转列表页面
+            this.$router.push({ path: "/yygh/hospset/list" });
+          });
+        }else{
+          this.$message.error("表单填写有误")
+          return false;
+        }
+      });
+    },
+  },
+};
 </script>
 
